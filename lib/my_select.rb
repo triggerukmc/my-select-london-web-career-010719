3af3665aa884082bc3dad63156adacd64f68642e
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-23 16:00:12 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,13 +1,12 @@
 def my_select(collection)
  iteration = 0
  array=[]
- while iteration > collection.length
+ while iteration < collection.length
  if yield(collection[iteration])
 array << yield(collection[iteration])
 end
 array
-iteration+=1 
-
+iteration+=1
 end
 array
 end
